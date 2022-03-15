c7675ddb598ae211b608ced089924fb07be1b5fa
maaravin <maaravin@LIN24008358.corp.capgemini.com>
2022-03-15 15:13:42 +0530

    helooo.......

---
+++ src/n1/Demo123.java
@@ -0,0 +1,19 @@
+package n1;
+
+public class Demo123 {
+
+	public static void main(String[] args) {
+		String name = "Aravind";
+		if(name=="Aravind") {
+			System.out.println("ture");
+		}
+	  else {
+		  System.out.println("false");
+	  }
+				
+		}
+		
+
+	
+
+}
